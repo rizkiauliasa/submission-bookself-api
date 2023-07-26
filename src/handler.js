2026e9d35fd2828ca804385f3cb8e44940ef7599
rizkiauliasa <rizkiauliasa@student.uns.ac.id>
2023-07-26 07:50:38 +0700

    Kriteria 6 : API dapat mengubah data buku

--- src/handler.js
+++ src/handler.js
@@ -96,13 +96,14 @@ const getBookByIdHandler = (request, h) => {
     const book = books.filter((b) => b.id === bookId)[0];
 
     if (book !== undefined) {
-        const response = {
+        const response = h.response({
             status: 'success',
             data: {
                 book
             }
-        };
+        });
 
+        response.code(200);
         return response;
     }
 
@@ -116,4 +117,57 @@ const getBookByIdHandler = (request, h) => {
     return response;
 };
 
-module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
+const editBookByIdHandler = (request, h) => {
+    const { bookId } = request.params;
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+    const updatedAt = new Date().toISOString();
+
+    const index = books.findIndex((book) => book.id === bookId);
+
+    if (!name) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Mohon isi nama buku'
+        });
+
+        response.code(400);
+        return response;
+    }
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+        });
+
+        response.code(400);
+        return response;
+    }
+
+    if (index !== -1) {
+        books[index] = {
+            ...books[index],
+            name, year, author, summary, publisher, pageCount, readPage, reading,
+            updatedAt
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui'
+        });
+
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan'
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler };
